f71d7c3e7fbc090584c06cd9f7aed034b37e8da8
Khris_Rod <74474330+khris-47@users.noreply.github.com>
2025-04-15 12:54:38 -0600

    Add files via upload

---
+++ Quiz02/Main.py
@@ -0,0 +1,36 @@
+from MetodoCesar import Cesar
+from BubbleSort import MetodoBurbuja
+
+def Main():
+
+    cesar = Cesar()
+
+    while True:
+        print("\n=====Bienvenido=====")
+        print("1.🥸  Cifrado Cesar")
+        print("2.🫧  Bubble Sort")
+        print("3.❎ Salir")
+
+        opcion = input("Que deseas realizar? ").strip()
+
+        if opcion == "1":
+            frase = input("🧐 Que frase o palabra deseas cifrar? ").strip()
+            desplazamiento = input("😅 Cuanto deseas desplazar? ").strip()
+            print(f"{cesar.cifrado(frase,desplazamiento)}") #llamo al metodo de cifrado, este devuelve un mensaje
+
+        elif opcion == "2":
+            lista = input("\nDame una lista de numeros (separada por ',' ): ").split(",")
+            lista_entero = list(map(int, lista)) #convierto lo ingresado a int
+
+            burbuja = MetodoBurbuja(lista_entero) #inicializo la clase metodoBurjuba (este requeria de la lista)
+            print(f"{burbuja.ordenar()}") #llamo al metodo, este devolvera un mensaje
+
+        elif opcion == "3":
+            print("✌️ Hasta luego, fue un placer tenerte aqui ✌️\n👋 Saliendo del programa...")
+            break
+
+        else:
+            print("🚫Upss!, Opcion no valida🚫")
+
+#---------------------------------------------------------------------------------------------------------------
+Main()
